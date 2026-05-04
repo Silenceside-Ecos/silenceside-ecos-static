08914b660631b857cdf86262643a9a632dfb277c
Carlos Iván García Delgado <carlygd8@gmail.com>
2026-05-04 19:04:21 -0400

    Configure custom domain silencesideecos.com, remove basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,11 @@
 ﻿/** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/silenceside-ecos-static',
-  env: {
-    NEXT_PUBLIC_BASE_PATH: '/silenceside-ecos-static',
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
-    loader: 'custom',
-    loaderFile: './lib/image-loader.ts',
+    unoptimized: true,
   },
 };
 
