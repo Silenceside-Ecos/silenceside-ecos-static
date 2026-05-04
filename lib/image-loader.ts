870be8ce2c1e911d1f1cdcb2b2391322fd12fb6b
Carlos Iván García Delgado <carlygd8@gmail.com>
2026-05-04 18:31:23 -0400

    Add missing image-loader.ts to main

--- lib/image-loader.ts
+++ lib/image-loader.ts
@@ -1,7 +1,13 @@
-export default function imageLoader({ src }: { src: string; width: number; quality?: number }) {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
-  if (src.startsWith('http') || src.startsWith('//')) {
-    return src
+export default function imageLoader({
+  src,
+}: {
+  src: string;
+  width: number;
+  quality?: number;
+}) {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+  if (src.startsWith("http") || src.startsWith("//")) {
+    return src;
   }
-  return `${basePath}${src}`
+  return `${basePath}${src}`;
 }
